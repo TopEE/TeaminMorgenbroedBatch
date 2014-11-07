9d32a659a6248fd1e922b45de97ddba4caff8ad6
hvagit <henrik1969@gmail.com>
2014-11-07 19:45:49 +0100

    Init revision

---
+++ src/java/com/teamin/batch/util/FileReadUtil.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamin.batch.util;
+
+/**
+ *
+ * @author Henrik
+ */
+public class FileReadUtil {
+    public int hentSaldo(String userId)
+    {
+        return 567;
+    }
+}
