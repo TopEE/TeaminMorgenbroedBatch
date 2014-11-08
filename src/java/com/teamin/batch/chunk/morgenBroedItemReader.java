0273cfe82da4cbe0737a18779e1b0c1f6b94daa9
hvagit <henrik1969@gmail.com>
2014-11-08 12:55:58 +0100

    Sender filnavn parameter

--- src/java/com/teamin/batch/chunk/morgenBroedItemReader.java
+++ src/java/com/teamin/batch/chunk/morgenBroedItemReader.java
@@ -5,9 +5,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.batch.api.chunk.AbstractItemReader;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.context.JobContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
@@ -15,10 +19,25 @@ public class morgenBroedItemReader extends AbstractItemReader {
 
     private BufferedReader reader;
     private Integer recordNumber;
+    
+        @Inject
+        private JobContext jobContext;
 
     @Override
     public void open(Serializable checkpoint) throws Exception {
-	reader = new BufferedReader(new FileReader("C:\\Temp\\testFiler\\morgenBroed.csv"));
+        
+       Properties jobParameters = BatchRuntime.getJobOperator().getParameters(jobContext.getExecutionId());
+
+        String filNavn = (String) jobParameters.get("fileName");
+
+        if(filNavn != null && !filNavn.isEmpty())
+        {
+        	reader = new BufferedReader(new FileReader("C:\\Temp\\testFiler\\"+filNavn));
+        }
+        else
+        {
+        	reader = new BufferedReader(new FileReader("C:\\Temp\\testFiler\\morgenBroed.csv"));
+        }
         
         if (checkpoint != null)
             recordNumber = (Integer) checkpoint;
