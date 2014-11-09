3836f1977d6e5e84238a32beb410569af361cb4b
hvagit <henrik1969@gmail.com>
2014-11-09 23:27:10 +0100

    Init

---
+++ src/java/com/teamin/batch/batchlet/InkassoMailBatchlet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamin.batch.batchlet;
+
+import javax.batch.api.AbstractBatchlet;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Henrik
+ */
+@Named
+public class InkassoMailBatchlet extends AbstractBatchlet {
+    
+    @Override
+    public String process() {
+        System.out.println("Running inside a batchlet : InkassoMailBatchlet");
+        // TODO code application logic here
+        return "COMPLETED";
+    }
+    
+}
