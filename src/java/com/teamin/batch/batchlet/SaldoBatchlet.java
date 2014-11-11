44f4aa1833910bc0cb709302147ad83c4e145b7e
hvagit <henrik1969@gmail.com>
2014-11-11 23:39:11 +0100

    Fjernet Dependent.

--- src/java/com/teamin/batch/batchlet/SaldoBatchlet.java
+++ src/java/com/teamin/batch/batchlet/SaldoBatchlet.java
@@ -11,7 +11,6 @@ import java.io.FileWriter;
 import java.util.List;
 import javax.batch.api.AbstractBatchlet;
 import javax.batch.runtime.context.StepContext;
-import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -21,7 +20,7 @@ import javax.persistence.PersistenceContext;
  *
  * @author Henrik
  */
-@Dependent
+
 @Named
 public class SaldoBatchlet extends AbstractBatchlet {
     
