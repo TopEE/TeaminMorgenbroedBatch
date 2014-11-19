c9154cf2525994160c769cf11eccee48bab88c52
hvagit <henrik1969@gmail.com>
2014-11-19 23:21:05 +0100

    Implementerer mail-afsendelse.

--- src/java/com/teamin/batch/batchlet/InfoMailBatchlet.java
+++ src/java/com/teamin/batch/batchlet/InfoMailBatchlet.java
@@ -1,12 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.teamin.batch.batchlet;
 
+import java.util.Properties;
 import javax.batch.api.AbstractBatchlet;
 import javax.inject.Named;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -15,10 +19,40 @@ import javax.inject.Named;
 @Named
 public class InfoMailBatchlet extends AbstractBatchlet {
     
+    private static String HOSTNAME = "localhost";
+    private static String USERNAME = "admin";
+    private static String PASSWORD = "henrik1969";
+
     @Override
     public String process() {
         System.out.println("Running inside a batchlet : InfoMailBatchlet");
-        // TODO code application logic here
+        
+        try {
+            String to1 = "hve@teamin.dk";    
+            String to2 = "jmn@teamin.dk";    
+            String to3 = "jhl@teamin.dk";    
+            String from = "hve@teamin.dk";
+            Properties properties = System.getProperties();
+
+            properties.setProperty("mail.smtp.host", HOSTNAME);
+            Session session = Session.getInstance(properties, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(USERNAME, PASSWORD);
+                }
+            });
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to1));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to2));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to3));
+            message.setSubject("Infomail");
+            message.setText("Du skylder os penge...");
+            Transport.send(message);
+            System.out.println("Mail er sendt");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
         return "COMPLETED";
     }
     
