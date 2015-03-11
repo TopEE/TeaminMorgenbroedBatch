da145bda31c3b81016425725e19af40db30f62d1
hvagit <henrik1969@gmail.com>
2015-03-11 21:57:37 +0100

    Init

--- src/java/com/teamin/mail/KanIkkeCompile.java
+++ src/java/com/teamin/mail/KanIkkeCompile.java
@@ -10,5 +10,5 @@ package com.teamin.mail;
  * @author Henrik
  */
 public class KanIkkeCompile {
-    kanIkkeCompile checkIndIgen og igen og igen og en gang til...og sidste gang
+    kanIkkeCompile checkIndIgen og igen og igen og en gang til...og igen
 }
